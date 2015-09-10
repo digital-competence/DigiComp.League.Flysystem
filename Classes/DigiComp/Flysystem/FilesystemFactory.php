ba7d249ac95f3f53d0c8e4da89db75dfab73500a
Ferdinand Kuhl <f.kuhl@digital-competence.de>
2015-09-10 12:28:55 +0200

    adding small documentation and flexibilized factory

--- Classes/DigiComp/Flysystem/FilesystemFactory.php
+++ Classes/DigiComp/Flysystem/FilesystemFactory.php
@@ -2,9 +2,12 @@
 namespace DigiComp\Flysystem;
 
 use Doctrine\ORM\Mapping as ORM;
+use League\Flysystem\AdapterInterface;
 use League\Flysystem\Filesystem;
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Configuration\Exception\InvalidConfigurationException;
 use TYPO3\Flow\Object\ObjectManagerInterface;
+use TYPO3\Flow\Reflection\ObjectAccess;
 use TYPO3\Flow\Reflection\ReflectionService;
 
 /**
@@ -19,12 +22,48 @@ class FilesystemFactory
      */
     protected $objectManager;
 
-    public function create($filesystemAdapterName, $filesystemProperties, $plugins = array()) {
-        $filesystem = new Filesystem(new $filesystemAdapterName($filesystemProperties));
+    /**
+     * @param array $filesystemAdapter
+     * @param array $plugins
+     *
+     * TODO: What to do with local filesystem?
+     *
+     * @return Filesystem
+     * @throws InvalidConfigurationException
+     */
+    public function create($filesystemAdapter, $plugins = array()) {
+        $adapterName = $filesystemAdapter['adapter'];
+        unset($filesystemAdapter['adapter']);
+
+        $class = new \ReflectionClass($adapterName);
+        $constructor = $class->getConstructor();
+
+        $arguments = [];
+        foreach($constructor->getParameters() as $parameter) {
+            if (isset($filesystemAdapter[$parameter->getName()])) {
+                $arguments[] = $filesystemAdapter[$parameter->getName()];
+                unset($filesystemAdapter[$parameter->getName()]);
+            } elseif(!$parameter->isOptional()) {
+                throw new InvalidConfigurationException('Missing Parameter of ' . $adapterName . ': ' . $parameter->getName());
+            }
+        }
+        /** @var AdapterInterface $adapter */
+        $adapter = $class->newInstanceArgs($arguments);
+
+        foreach($filesystemAdapter as $key => $val) {
+            if (ObjectAccess::isPropertySettable($adapter, $key)) {
+                ObjectAccess::setProperty($adapter, $key, $val);
+            } else {
+                throw new InvalidConfigurationException('Could not set Parameter ' . $key . ' to ' . $adapterName);
+            }
+        }
+
+        $filesystem = new Filesystem($adapter);
         foreach($plugins as $plugin) {
             $pluginClass = $this->resolvePlugin($plugin);
             $filesystem->addPlugin(new $pluginClass());
         }
+
         return $filesystem;
     }
 
@@ -40,7 +79,15 @@ class FilesystemFactory
         return array_flip($classNames);
     }
 
-    public function resolvePlugin($pluginName) {
+    /**
+     * Searches for plugin classes, resolves Plugins without Package identifier in League\Flysystem
+     * Names like DigiComp.Package:Special are resolved to \DigiComp\Package\FlysystemPlugin\Special
+     *
+     * @param string $pluginName
+     *
+     * @return bool|string
+     */
+    protected function resolvePlugin($pluginName) {
         $plugins = $this->getPlugins($this->objectManager);
         if (strpos($pluginName, ':') !== FALSE) {
             list($packageName, $packagePluginName) = explode(':', $pluginName);
---
+++ README.md
@@ -0,0 +1,46 @@
+DigiComp.League.Flysystem
+-------------------------
+
+
+This package contains a factory for the filesystem abstraction layer written by Frank de Jonge. 
+See http://flysystem.thephpleague.com/
+
+To use a filesystem in your project, I suggest you create an interface:
+
+	/**
+	 * @method array listFiles(string $path = '', boolean $recursive = false)
+     * @method array listPaths(string $path = '', boolean $recursive = false)
+	 */
+    interface MyFilesystemInterface extends FilesystemInterface {}
+
+And configure it with your Objects.yaml:
+
+    AcMe\Package\SourceFilesystemInterface:
+      scope: singleton
+      factoryObjectName: DigiComp\Flysystem\FilesystemFactory
+      factoryMethodName: create
+      arguments:
+        1:
+          setting: 'AcMe.Package.filesystem'
+        2:
+          value: ["ListPaths", "ListFiles"]
+
+While your Settings.yaml could look like this:
+
+	AcMe:
+	  Package:
+		filesystem:
+		  adapter: League\Flysystem\Adapter\Ftp
+		  config:
+			host: digital-competence.de
+			username: user
+			password: password
+			root: /path/for/root
+			
+The array used as first parameter for the factory expects a "adapter" - key and all constructor arguments needed for the
+  instantiation of the configured adapter. All other keys will be treated as property setters.
+   
+The second parameter is a list of plugins, which should be added to your filesystem. Without a package prefix, they will
+be searched in League\Flysysten\Plugin. If you write something like "AcMe.Package:MyPlugin" the factory will look for an
+plugin named \AcMe\Package\FlysystemPlugin\MyPlugin.
+
